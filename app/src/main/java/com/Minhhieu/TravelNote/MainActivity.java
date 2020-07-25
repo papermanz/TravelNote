04e9daecfe7360ec00e8a991c2c739c3e48f3c4f
minhhieu <kaitakenzo@gmail.com>
2020-07-26 00:40:35 +0700

    action bar

--- app/src/main/java/com/Minhhieu/TravelNote/MainActivity.java
+++ app/src/main/java/com/Minhhieu/TravelNote/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+    //sử dụng Main action làm menu của actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //thông báo khi chọn button trên action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+       switch (item.getItemId()){
+           case R.id.Search:
+               Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
+                return true;
+           case R.id.about:
+               Toast.makeText(this, "About button selected", Toast.LENGTH_SHORT).show();
+               return true;
+           case R.id.help:
+               Toast.makeText(this,"Help button selected",Toast.LENGTH_SHORT).show();
+               return true;
+       }
+       return super.onOptionsItemSelected(item);
     }
 }
