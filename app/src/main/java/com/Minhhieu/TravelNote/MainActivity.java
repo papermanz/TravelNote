040252c52a22736652c71102efdb9d8283e1d9b4
minhhieu <kaitakenzo@gmail.com>
2020-07-26 21:12:29 +0700

    navigation Drawer

--- app/src/main/java/com/Minhhieu/TravelNote/MainActivity.java
+++ app/src/main/java/com/Minhhieu/TravelNote/MainActivity.java
@@ -1,7 +1,10 @@
 package com.Minhhieu.TravelNote;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.res.Configuration;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +14,33 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //con trỏ đến activity drawer
+        drawerLayout = findViewById(R.id.activity_main_drawer);
+        //điều khiển việc đóng mở action bar
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
 
-
+        //icon hamburger vào actionbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
     //sử dụng Main action làm menu của actionbar
     @Override
@@ -28,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
     //thông báo khi chọn button trên action bar
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
        switch (item.getItemId()){
            case R.id.Search:
                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
